89255f7b2b1e0fd9dece69be6779a6b7cf66219d
gannonsun <sungangnian@12301.cc>
2021-11-19 18:13:08 +0800

    Image: delete play/index.vue

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,27 +1,15 @@
 <template>
-    <div style="margin: 20px;">
-        <el-image
-            style="width: 100px; height: 100px"
-            :src="url"
-            :preview-src-list="srcList"
-            :append-to-body="false"
-            :maskClosable="false"
-        >
-        </el-image>
-    </div>
+  <div style="margin: 20px;">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            url:
-                "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
-            srcList: [
-                "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
-                "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg",
-            ],
-        };
-    },
-};
+      return {
+        input: 'Hello Element UI!'
+      };
+    }
+  };
 </script>
